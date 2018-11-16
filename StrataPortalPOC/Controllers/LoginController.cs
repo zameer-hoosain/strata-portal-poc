4acab9ff9c4354ef86249c3b7cd175f062ee82f2
Zameer Hoosain <zameer@geminisolution.co.za>
2018-11-16 11:58:31 +0200

    Add project skeleton

---
+++ StrataPortalPOC/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StrataPortalPOC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET api/login
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
